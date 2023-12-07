848986903855ec94f4c42b4324294cdee8497296
kevin.nava@externos-cl.cencosud.cl <kevin.nava@externos-cl.cencosud.com.com>
2023-12-07 19:11:46 +0100

    ID assignment

--- main.m
+++ main.m
@@ -1,40 +1,77 @@
+% % Create a VideoReader object to read the MP4 file
+% videoObj = VideoReader('/Users/appleuser/Desktop/pazmany/algorithms_image_processing/assignment_2/video/input_video.mp4');
+% 
+% % Create a People Detector object
+% peopleDetector = vision.PeopleDetector;
+% 
+% colors = {'red','yellow','green','blue','orange','purple'}
+% 
+% % Initialize array to store Kalman filters for each detected person
+% kalmanFilters = [];
+% 
+% % Read and process each frame of the video
+% while hasFrame(videoObj)
+%     frame = readFrame(videoObj);  % Read a single frame
+% 
+%     % Pre-processing to improve image quality
+%     frame = histeq(frame);  % Improve contrast
+%     frame = imgaussfilt(frame, 2);  % Apply Gaussian blur for noise reduction
+% 
+%     [bboxes, scores] = peopleDetector.step(frame); % Detect people
+% 
+%     % Update or initialize Kalman filters for detected people
+%     kalmanFilters = updateKalmanFilters(kalmanFilters, bboxes);
+% 
+%     % Draw bounding boxes around detected or predicted people
+%     % frame = insertObjectAnnotation(frame, 'rectangle', bboxes, scores,'Color','red');
+%     % Annotate frame with colored bounding boxes
+%     for i = 1:size(bboxes, 1)
+%         disp(colors{i})
+%         frame = insertObjectAnnotation(frame, 'rectangle', bboxes(i,:), scores(i), 'Color', colors{i});
+%     end
+% 
+%     imshow(frame); % Display the annotated frame
+%     pause(1/videoObj.FrameRate); % Pause for the duration of one frame
+% end
 
 
-
-% Create a VideoReader object to read the MP4 file
+% Crear un VideoReader para leer el archivo MP4
 videoObj = VideoReader('/Users/appleuser/Desktop/pazmany/algorithms_image_processing/assignment_2/video/input_video.mp4');
 
-% Create a People Detector object
+% Crear un detector de personas
 peopleDetector = vision.PeopleDetector;
 
-colors = {'red','yellow','green','blue','orange','purple'}
+% Inicializar colores
+colors = {'red', 'yellow', 'green', 'blue', 'orange', 'purple'};
 
-% Initialize array to store Kalman filters for each detected person
+% Inicializar contenedores para Kalman Filters y IDs
 kalmanFilters = [];
+personIDs = [];
+nextID = 1;
 
-% Read and process each frame of the video
+% Leer y procesar cada fotograma del video
 while hasFrame(videoObj)
-    frame = readFrame(videoObj);  % Read a single frame
+    frame = readFrame(videoObj); % Leer un solo fotograma
 
-    % Pre-processing to improve image quality
-    frame = histeq(frame);  % Improve contrast
-    frame = imgaussfilt(frame, 2);  % Apply Gaussian blur for noise reduction
+    % Pre-procesamiento para mejorar la calidad de la imagen
+    frame = histeq(frame); % Mejorar contraste
+    frame = imgaussfilt(frame, 2); % Aplicar desenfoque gaussiano
 
-    [bboxes, scores] = peopleDetector.step(frame); % Detect people
+    [bboxes, scores] = peopleDetector.step(frame); % Detectar personas
 
-    % Update or initialize Kalman filters for detected people
-    kalmanFilters = updateKalmanFilters(kalmanFilters, bboxes);
-    
-    % Draw bounding boxes around detected or predicted people
-    % frame = insertObjectAnnotation(frame, 'rectangle', bboxes, scores,'Color','red');
-    % Annotate frame with colored bounding boxes
-    for i = 1:size(bboxes, 1)
-        disp(colors{i})
-        frame = insertObjectAnnotation(frame, 'rectangle', bboxes(i,:), scores(i), 'Color', colors{i});
+    % Actualizar o inicializar Kalman filters y IDs para personas detectadas
+    [kalmanFilters, personIDs, nextID] = updateKalmanFilters(kalmanFilters, personIDs, bboxes, nextID);
+
+    % Dibujar bounding boxes con ID y color para cada persona
+    for i = 1:min(length(personIDs), size(bboxes, 1))
+        colorIndex = mod(personIDs(i) - 1, length(colors)) + 1;
+        label = sprintf('ID: %d', personIDs(i));
+        frame = insertObjectAnnotation(frame, 'rectangle', bboxes(i,:), label, 'Color', colors{colorIndex});
     end
 
-    imshow(frame); % Display the annotated frame
-    pause(1/videoObj.FrameRate); % Pause for the duration of one frame
+
+    imshow(frame); % Mostrar el fotograma anotado
+    pause(1/videoObj.FrameRate); % Pausar por la duración de un fotograma
 end
 
 
--- updateKalmanFilters.m
+++ updateKalmanFilters.m
@@ -1,20 +1,46 @@
-function [kalmanFilters, colors] = updateKalmanFilters(kalmanFilters, bboxes, colors)
-    % Function to update or initialize Kalman filters and assign colors
+% function [kalmanFilters, colors] = updateKalmanFilters(kalmanFilters, bboxes, colors)
+%     % Function to update or initialize Kalman filters and assign colors
+%     for i = 1:size(bboxes, 1)
+%         bbox = bboxes(i, :);
+%         if i > numel(kalmanFilters)
+%             % Initialize a new Kalman filter for this detection
+%             kalmanFilter = configureKalmanFilter('ConstantVelocity', ...
+%                                                  bbox(1:2), [1 1]*1e5, ...
+%                                                  [25, 10], 25);
+%             kalmanFilters{i} = kalmanFilter;
+% 
+%             % Assign a random color for this filter
+%             colors{i} = rand(1, 3);
+%         else
+%             % Update existing Kalman filter with new detection
+%             kalmanFilter = kalmanFilters{i};
+%             bbox = correct(kalmanFilter, bbox(1:2));
+%         end
+%     end
+% end
+
+function [kalmanFilters, personIDs, nextID] = updateKalmanFilters(kalmanFilters, personIDs, bboxes, nextID)
+    % Función para actualizar o inicializar los filtros de Kalman y asignar IDs
+    numAssigned = 0; % Contador para las detecciones ya asignadas
+
     for i = 1:size(bboxes, 1)
         bbox = bboxes(i, :);
-        if i > numel(kalmanFilters)
-            % Initialize a new Kalman filter for this detection
+
+        if numAssigned < numel(kalmanFilters)
+            % Actualizar filtro de Kalman existente con nueva detección
+            kalmanFilter = kalmanFilters{numAssigned + 1};
+            bbox = correct(kalmanFilter, bbox(1:2));
+            numAssigned = numAssigned + 1;
+        else
+            % Inicializar un nuevo filtro de Kalman para esta detección
             kalmanFilter = configureKalmanFilter('ConstantVelocity', ...
                                                  bbox(1:2), [1 1]*1e5, ...
                                                  [25, 10], 25);
-            kalmanFilters{i} = kalmanFilter;
-            
-            % Assign a random color for this filter
-            colors{i} = rand(1, 3);
-        else
-            % Update existing Kalman filter with new detection
-            kalmanFilter = kalmanFilters{i};
-            bbox = correct(kalmanFilter, bbox(1:2));
+            kalmanFilters{end + 1} = kalmanFilter;
+
+            % Asignar un nuevo ID para esta detección
+            personIDs(end + 1) = nextID;
+            nextID = nextID + 1;
         end
     end
 end
